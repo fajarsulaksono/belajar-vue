aaf2e3a4a772a6390c39ca90f2279e1521a365ac
Fajar Sulaksono <fajar.sulaksono@gmail.com>
2020-04-18 19:01:35 +0700

    Rename file

--- 01-vue-mastery-intro-to-vue/02-attribute-binding/js/02-main.js
+++ 01-vue-mastery-intro-to-vue/02-attribute-binding/js/02-main.js
@@ -1,6 +1,9 @@
 var app = new Vue({
     el: '#app',
     data: {
-        product: 'Socks'
+        product: 'Socks',
+        image: './assets/vmSocks-blue-onWhite.jpg',
+        altText: 'A pair of socks',
+        linkToGoogle: 'https://google.com'
     }
 })
